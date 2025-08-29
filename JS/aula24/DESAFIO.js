e05d571503e3a47abc26347eb847ea37933a09d4
techcomkathia <techcomk@gmail.com>
2025-08-28 22:00:07 -0300

    atividade 2 e desafio

---
+++ JS/aula24/DESAFIO.js
@@ -0,0 +1,6 @@
+// de acordo com a atividade 2, faça a evolução da questão para
+//as atividades serão adicionadas em 2 arrays, urgentes e naoUrgentes de acordo com a informação do usuario
+// em uma lista ordenada, exiba todas as atividades urgentes antes das não urgentes. 
+// as atividades urgentes terão fonte vermelha
+// as atividades nao urgentes terão fonte azul
+//( crie css novo para as classes)
--- JS/aula24/atividade2.js
+++ JS/aula24/atividade2.js
@@ -2,13 +2,32 @@
 //urgente : bg vermelho, fonte branca e negrito
 //naoUrgente: fonte preta e bg verde claro
 
-//nor arquivo de script crie um programa que execute a seguinte tarefa:
+//no arquivo de script crie um programa que execute a seguinte tarefa:
 //peça ao usuário a quantidade de atividades para o dia
+let qtdAtividades = prompt('Digite quantas atividades teremos para o dia de hoje')
+//selecionando a tag que receberá a lista
+let main = document.querySelector('main')
+//criando a lista e adicionando na tag pai
+let ul = document.createElement('ul')
+main.appendChild(ul)
+
 //a cada atividade faça 2 perguntas:
-//o texto da atividade
-//se a atividade é urgente ou não.
-
-//se atividade for urgente, crie o elemento, adicione o texto, insira na lista de classes a classe urgente
-// se a atividade não for urgente faça o mesmo processo mas adicione a classe naoUrgente.
-
-// * dica: utilize os métodos createElement, appendChild, classList.add()
+for(let i=1; i<= qtdAtividades; i++){
+    //o texto da atividade
+    let textoAtividade = prompt(`Digite a atividade ${i} de ${qtdAtividades}`)
+    //se a atividade é urgente ou não.
+    let ehUrgente= confirm(`A atividade ${i} é urgente? Se sim, pressione ok, se não cancelar.`)
+    //se atividade for urgente, crie o elemento, adicione o texto, insira na lista de classes a classe urgente
+    // se a atividade não for urgente faça o mesmo processo mas adicione a classe naoUrgente.
+    //condicional para criar a atividade de acordo com a necessidade
+    let item = document.createElement('li')
+    item.innerHTML = textoAtividade
+    //identicar a urgência para adicionar a classe correta
+    if (ehUrgente) {
+        item.classList.add('urgente')
+    } else {
+        item.classList.add('naoUrgente')
+    }
+    ul.appendChild(item)
+    // * dica: utilize os métodos createElement, appendChild, classList.add()  
+}
